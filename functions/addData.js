1ffd0c1ab0729b0f5f841babd958f075c7ab246f
root <TikTok@example.com>
2021-04-22 08:24:32 +0000

    Step 7 final

---
+++ functions/addData.js
@@ -0,0 +1,35 @@
+const fs = import('fs')
+const { createClient } = require("@astrajs/collections")
+
+const collection = 'posts'
+
+exports.handler = async function (event, context, callback) {
+    const fs = require('fs')
+      const astraClient = await createClient({
+       astraDatabaseId: process.env.ASTRA_DB_ID,
+       astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+       applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+     });
+
+  const posts = astraClient
+    .namespace(process.env.ASTRA_DB_KEYSPACE)
+    .collection(collection)
+
+  const data = JSON.parse(fs.readFileSync('/root/data.json'))
+    
+  try {
+        for (let i = 0; i < data.length; i++) {
+          await posts.create(data[i].id.toString(), data[i])
+        }
+
+        return {
+            statusCode: 200,
+        }
+    } catch (e) {
+        console.error(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e),
+        }
+    }
+}   
